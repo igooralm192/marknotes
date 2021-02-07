64e20f725bb90556451259ab6a27593d7a98490d
Igor de Almeida <igoor1205@gmail.com>
2021-02-07 19:10:22 -0300

    :construction: Finish init config

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <span>Hello World</span>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
-  },
-});
+})
 </script>
 
 <style>
@@ -20,8 +15,5 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
